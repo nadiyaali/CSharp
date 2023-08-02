54854e601bc6d58f83af059d4816dd4eaf07bc41
Nadia <nadiya.ali@gmail.com>
2023-08-02 11:04:26 +1000

    Simple MVC app with Azure SQL Server as db

---
+++ Projects/WebAppAzureSqlServerSln/WebAppAzureSqlServer/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿namespace WebAppAzureSqlServer.Models
+{
+    public class Course
+    {
+        // This class is a representation of the structure of our data
+        public int CourseID { get; set; }
+        public string CourseName { get; set; }
+        public decimal Rating { get; set; }
+
+    }
+}
+
+
+/* SQL
+
+
+ create table course
+(
+ CourseID int,
+ CourseName varchar(1000),
+ Rating numeric(2,1)
+)
+
+insert into course values(1,'AZ-900 Azure Fundamentals',2.0)
+
+insert into course values(2,'AZ-204 Developing Solutions for MS Azure',5.0)
+insert into course values(3,'AZ-104 MS Azure Administrator',5.0)
+
+select * from course
+
+update course
+set Rating = 4.7
+where CourseID = 3 
+ 
+ */
